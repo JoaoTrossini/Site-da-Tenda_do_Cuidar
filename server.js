0a018c5ce523c143b91d1fac301b4df783a93db7
Joao Trossini <joaotrossini22@gmail.com>
2026-08-18 13:47:47 -0300

    Corrige inicializacao do servidor

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
-
 const path = require("path");
-
-app.use(cors());
-app.use(express.json());
-
-app.use(express.static(__dirname));
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 
 // =========================================================
