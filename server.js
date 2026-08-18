e95ca00a4f8915620793f0929b3cc59acc86d142
João Trossini <joaotrossini22@gmail.com>
2026-08-17 23:20:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,14 @@ app.use(express.json());
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 20179),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
 
     waitForConnections: true,
     connectionLimit: 10,
