9b2615e1d6bd683cc43fc043e1e331c684d93718
João Trossini <joaotrossini22@gmail.com>
2026-08-17 23:07:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(express.json());
 // ======================================================
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "0422",
-    database: "tenda_cuidar",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
